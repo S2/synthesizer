4cae971b6b6d41bf51d92cb43ca4a8e78a3a446b
sato <s2otsa59@gmail.com>
2020-01-17 00:15:03 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+var webpack = require('webpack');
+
+let entry = {
+    "index.js" : [
+        "./src/Index.ts" , 
+    ] , 
+}
+
+let plugins = [
+    new webpack.LoaderOptionsPlugin({
+        options: {
+            postcss: [
+            ]
+        }
+    })
+];
+
+module.exports = {
+    cache:true , 
+    entry: entry ,
+    output: {
+        filename: "[name].js",
+        path: __dirname + "/dist/"
+    },
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }]
+            },
+            {
+                test: /\.sass$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" 
+                }, {
+                    loader: "import-glob-loader" 
+                }]
+            },
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader" ,
+                options: {
+                },
+            },
+            {
+                test: /\.(jpg|png|woff|woff2|eot|otf|ttf|svg)$/,
+                loader: 'url-loader?limit=100000&name=/[hash].[ext]'
+            }
+        ]
+    },
+    plugins: plugins , 
+};
